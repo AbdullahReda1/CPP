e4b7a2fbbf8dd9e17313b657cac85514a7b49917
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-08-22 23:55:16 +0300

    S6

--- Solos/Friend/friend.cpp
+++ Solos/Friend/friend.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 
-class B; // Forward declaration
+/* Forward declaration */
+class B;
 
 class A
 {
     private:
         int privateDataA;
+        
+        /* Declare B as a "friend" class which make "its all members" accessable */
+        friend class B;
 
-        friend class B;         // Declare B as a "friend" class which make `its all members` accessable
-
-        friend void printPrivateData(const A& obj);     // Declare a non-member function as a "friend"
+        /* Declare a non-member function as a "friend" */
+        friend void printPrivateData(const A& obj);
 
     public:
         A(int data) : privateDataA(data) {}
 
-        // Overloaded operator << as a "friend" function
+        /* Overloaded operator `<<` as a "friend" function */
         friend ostream& operator <<(ostream& os, const A& obj) { os << "Private Data A: \t\t" << obj.privateDataA; return os; }
 };
 
@@ -31,10 +34,12 @@ class B
 
         void accessDataA(const A& obj) { cout << "Private Data A accessed from B: " << obj.privateDataA << endl; }
 
-        // Class B can access both the private members of class A and - cascade down - its own private members.
+        /* Class B can access both the private members of class A and - cascade down - its own private members */
         void accessDataB(const B& obj) { cout << "Private Data B accessed from B: " << obj.privateDataB << endl; }
 };
 
+/// @brief Function to print private data from class `A` which is a non-member friend function
+/// @param obj class object
 void printPrivateData(const A& obj) { cout << "Printing private data of A:     " << obj.privateDataA << endl; }
 
 
