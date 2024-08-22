83fadb1202ac2eb7335074f364fcacb28f9e72fe
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-08-23 00:41:15 +0300

    S6

--- Solos/Friend/friend.cpp
+++ Solos/Friend/friend.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-/* Forward declaration */
+/// @brief Forward declaration
 class B;
 
 class A
@@ -11,30 +11,42 @@ class A
     private:
         int privateDataA;
         
-        /* Declare B as a "friend" class which make "its all members" accessable */
+        /// @brief Declare B as a "friend" class which make "its all members" accessable
         friend class B;
 
-        /* Declare a non-member function as a "friend" */
+        /// @brief Declare a non-member function as a "friend"
+        /// @param obj class object
         friend void printPrivateData(const A& obj);
 
     public:
+        /// @brief class `A` with setting its private data A
+        /// @param data 
         A(int data) : privateDataA(data) {}
 
-        /* Overloaded operator `<<` as a "friend" function */
+        /// @brief Overloaded operator `<<` as a "friend" function
+        /// @param os output streams
+        /// @param obj the class object bassed to print its private data
+        /// @return message "Private Data A: `privateDataA`" 
         friend ostream& operator <<(ostream& os, const A& obj) { os << "Private Data A: \t\t" << obj.privateDataA; return os; }
 };
 
+/// @brief Class B can access both the private members of class A and - cascade down - its own private members
 class B
 {
     private:
         int privateDataB;
 
     public:
+        /// @brief class `B` with setting its private data B
+        /// @param data
         B(int data) : privateDataB(data) {}
 
+        /// @brief Check function if private data A of class A accessed from class B
+        /// @param obj the class object
         void accessDataA(const A& obj) { cout << "Private Data A accessed from B: " << obj.privateDataA << endl; }
 
-        /* Class B can access both the private members of class A and - cascade down - its own private members */
+        /// @brief Check function if private data B of class B accessed from class B
+        /// @param obj the class object
         void accessDataB(const B& obj) { cout << "Private Data B accessed from B: " << obj.privateDataB << endl; }
 };
 
