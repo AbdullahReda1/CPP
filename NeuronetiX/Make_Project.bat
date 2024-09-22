5726990e90618342b3d7825045a8f65647c897aa
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-09-23 00:04:58 +0300

    N_BAT

--- NeuronetiX/Make_Project.bat
+++ NeuronetiX/Make_Project.bat
@@ -2,6 +2,16 @@
 set /p directoryWeek="Please enter the Number of week to make project folders: "
 set /p directoryProj="Please enter the project name: "
 mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\build"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\doc"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\include"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\src"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\tests"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\third-party"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\ui"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\CMakeLists.txt"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\src\main.cpp"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\tests\unit_tests.cpp"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\ui\main.cpp"
 echo Done
 pause
