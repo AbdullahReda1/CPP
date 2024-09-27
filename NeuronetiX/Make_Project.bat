5c377e2f6aaf78fa0862e44d5f2d17c8f9cbed82
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-09-28 02:50:32 +0300

    N_BAT

--- NeuronetiX/Make_Project.bat
+++ NeuronetiX/Make_Project.bat
@@ -1,32 +1,30 @@
 @echo off
 
 
-set /p directoryWeek="Please enter the Number of week to make project folders: "
 set /p directoryProj="Please enter the project name: "
 
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\build"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\doc"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\include"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\src"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\tests"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\third-party"
-mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\ui"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\build"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\doc"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\include"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\src"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\tests"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\third-party"
+mkdir "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\ui"
 
 echo Done.
 
 REM Create an empty CMakeLists.txt file
-echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\CMakeLists.txt"
+echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\CMakeLists.txt"
 
 REM Create an empty src/main.cpp file
-echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\src\main.cpp"
+echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\src\main.cpp"
 
 REM Create an empty tests/unit_tests.cpp file
-echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\tests\unit_tests.cpp"
+echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\tests\unit_tests.cpp"
 
 REM Create an empty ui/main.cpp file
-echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\Week%directoryWeek%\%directoryProj%\ui\main.cpp"
+echo > "E:\ITI ES\10. C++\CPP\NeuronetiX\%directoryProj%\ui\main.cpp"
 
 
 pause
