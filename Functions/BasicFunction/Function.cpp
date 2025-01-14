0b9fd850905ca38c9e058e704104a4727b5daadd
AbdullahReda1 <abdullah.reda44@gmail.com>
2025-01-14 20:18:40 +0200

    F1. Implement sum function and update main to calculate and display the sum of two integers

--- Functions/BasicFunction/Function.cpp
+++ Functions/BasicFunction/Function.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 
 
+// Function declaration
+int sum(int a, int b);
+
+
 int main() {
-    
+    int num1, num2, Result;
+    cout << "Enter two int numbers" << endl << "---------------------" << endl;
+    cout << "Enter first numbers: ";
+    cin >> num1;
+    cout << "Enter second numbers: ";
+    cin >> num2;
+
+    // Function call
+    /*
+        the cpu will jump to the function definition and execute the code inside the function
+        then return the result to the main function and continue the execution of the main function.
+    */
+    Result = sum(num1, num2);
+
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << Result << endl;
+    return 0;
+}
+
+
+// Function definition
+int sum(int a, int b) {
+    return (a + b);
 }
