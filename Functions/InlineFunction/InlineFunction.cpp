0ee1a2e897d072ed362aa32c4eb0f1c4b3c6e6b7
Abdullah <abdullah.reda44@gmail.com>
2025-03-23 21:58:17 -0700

    Implement inline functions for addition and multiplication in InlineFunction.cpp

--- Functions/InlineFunction/InlineFunction.cpp
+++ Functions/InlineFunction/InlineFunction.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 
+// Inline function; helps to reduce the function call overhead
+// Inline function is a function that is expanded in line when it is called.
+// When the inline function is called whole code of the inline function gets inserted or expanded at the point of inline function call.
+// This expansion is performed by the compiler at the compile time. Inline function may increase efficiency if it is small.
+inline int add(int a, int b) { return a + b; }
+
+// Prototyping an inline function
+inline int multiply(int a, int b);
+
 int main() {
-    
+    int a, b;
+    cout << "Enter two numbers: " << endl;
+    cin >> a;
+    cin >> b;
+
+    // Calling inline function
+    cout << "Sum: " << add(a, b) << endl;
+    cout << "Product: " << multiply(a, b) << endl;
+
+    return 0;
 }
+
+
+// Inline function definition
+inline int multiply(int a, int b) { return a * b; }
