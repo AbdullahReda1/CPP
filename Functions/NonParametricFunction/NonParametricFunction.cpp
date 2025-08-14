a4c41342b4ace3759fb6b26640ba80d81d6f7638
Abdullah <abdullah.reda44@gmail.com>
2025-08-14 22:20:31 +0300

    Implement non-parametric functions for greeting and number retrieval in NonParametricFunction.cpp

--- Functions/NonParametricFunction/NonParametricFunction.cpp
+++ Functions/NonParametricFunction/NonParametricFunction.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// Non-parametric function (no parameters) returning void
+void greeting() {
+    cout << "Hello from a non-parametric function!" << endl;
+}
+
+// Non-parametric function returning int
+int getNumber() {
+    return 42;
+}
+
+
+int main() {
+    greeting();                     // Call the function
+    cout << getNumber() << endl;    // Outputs: 42
+    return 0;
+}
