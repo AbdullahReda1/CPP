c6eca3f870782d0a0bddafbf27a24fec2d0689c6
Abdullah <abdullah.reda44@gmail.com>
2025-08-29 19:47:44 +0300

    Update

---
+++ OOP/Fundamentals/Encapsulation/AccessModifiers/AccessModifiers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+class Data {
+    private:
+        string CryptoMSG = "97F2A05A";
+    protected:
+
+    public:
+
+};
+
+
+int main() {
+
+    return 0;
+}
