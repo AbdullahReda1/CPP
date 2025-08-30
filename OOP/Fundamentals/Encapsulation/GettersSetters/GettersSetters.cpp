23270f89e648ed829164d7a01fcb9e97f8c76030
Abdullah <abdullah.reda44@gmail.com>
2025-08-30 03:27:26 +0300

    Add GettersSetters.cpp to demonstrate encapsulation and data hiding concepts in C++

---
+++ OOP/Fundamentals/Encapsulation/GettersSetters/GettersSetters.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+
+class Data {
+    private:
+        string CryptoMSG = "97F2A05A";
+        int    Password  = 123       ;
+
+    protected:
+        float PublicData;
+        int ProtectivID = 102;
+
+        int getPassword(void) {
+            return Password;
+        }
+
+        int inputPassword(void) {
+            int pw;
+            cout << "Enter the password" << endl;
+            cin >> pw;
+            return pw;
+        }
+
+        string getCryptoMSG(void) {
+            return CryptoMSG;
+        }
+
+    public:
+        void setPublicData(float d) {
+            PublicData = d;
+        }
+
+        float getPublicData(void) {
+            return PublicData;
+        }
+};
+
+class BankData : public Data {
+    private:
+        bool checkPassword (int PW) {
+            return (getPassword() == PW);
+        }
+
+    public:
+        int getProtectivID(void) {
+            return ProtectivID;
+        }
+
+        string getCryptoMSG (void){
+            if(checkPassword (inputPassword()))
+                return (Data::getCryptoMSG());
+            else {
+                //string STR = "NOT CORRECT";
+                return "NOT CORRECT";
+            }
+        }
+};
+
+
+int main() {
+    BankData b;
+
+    b.setPublicData(5.06);
+    b.getPublicData();
+
+    b.getProtectivID();
+
+    b.getCryptoMSG();
+
+    return 0;
+}
