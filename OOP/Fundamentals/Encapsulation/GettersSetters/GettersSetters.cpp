0582725e172805b3d093dc126567fd59eb6613c5
Abdullah <abdullah.reda44@gmail.com>
2025-08-31 01:23:42 +0300

    Refactor comments in GettersSetters.cpp for improved clarity and consistency

--- OOP/Fundamentals/Encapsulation/GettersSetters/GettersSetters.cpp
+++ OOP/Fundamentals/Encapsulation/GettersSetters/GettersSetters.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 
 class Data {
+    // Only accessible inside Data
     private:
         string CryptoMSG = "97F2A05A";
-        int    Password  = 123       ;
+        int    Password  = 123;
 
+    // Accessible in Data and BankData
     protected:
         float PublicData;
         int ProtectivID = 102;
 
+        // Accessible in Data and BankData
         int getPassword(void) {
             return Password;
         }
 
+        // Accessible in Data and BankData
         int inputPassword(void) {
             int pw;
             cout << "Enter the password" << endl;
@@ -22,33 +26,43 @@ class Data {
             return pw;
         }
 
+        // Accessible in Data and BankData
         string getCryptoMSG(void) {
             return CryptoMSG;
         }
 
+    // Accessible from anywhere (main, etc.)
     public:
         void setPublicData(float d) {
             PublicData = d;
         }
 
+        // Accessible from anywhere (main, etc.)
         float getPublicData(void) {
             return PublicData;
         }
 };
 
 class BankData : public Data {
+    // Only accessible inside BankData
     private:
         bool checkPassword (int PW) {
+            // Calls protected getPassword() from Data
             return (getPassword() == PW);
         }
 
+    // Accessible from anywhere (main, etc.)
     public:
         int getProtectivID(void) {
+            // Accesses protected member from Data
             return ProtectivID;
         }
 
+        // Accessible from anywhere (main, etc.)
         string getCryptoMSG (void){
+            // Calls private checkPassword() from DataBank and protected inputPassword() from Data
             if(checkPassword (inputPassword()))
+                // Calls protected getCryptoMSG() from Data
                 return (Data::getCryptoMSG());
             else 
                 return "NOT CORRECT";
