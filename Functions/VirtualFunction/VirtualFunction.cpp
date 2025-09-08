0a3fd428ab6e86fc159dc980f9b823087f4c2b2a
Abdullah Reda <115156224+AbdullahReda1@users.noreply.github.com>
2025-09-08 16:53:26 +0300

    Updating to the virtual function file

--- Functions/VirtualFunction/VirtualFunction.cpp
+++ Functions/VirtualFunction/VirtualFunction.cpp
@@ -5,11 +5,27 @@ using namespace std;
 class Base {
     public:
 
-        virtual void VirtualFunctionInterface(void) {
+        virtual void FunctionInterface(void) {
             cout << "This is a calling to the virtual function interface from Base class" << endl;
         }
 
-        virtual int VirtualFunctionBlueprint() {
-
+        virtual string FunctionBlueprint() {
+            return "This is a calling to the virtual function blueprint from Base class \n";
         }
 };
+
+class Derived : public Base {
+    public:
+    void FunctionInterface() {
+        cout << "A message from Derived Interface function" << endl;
+    }
+
+    void FunctionBlueprint() {
+        return "A message from Derived blueprint function \n"
+    }
+};
+
+int main(void) {
+    
+    return 0;
+}
